a3f917d811fb040bdc2429c02428fd5ef5f0025f
mvansegbroeck <mvansegbroeck@gmail.com>
2023-11-16 15:41:52 -0800

    Update __init__.py

--- src/gretel_mlops/gcp/vertexai/components/__init__.py
+++ src/gretel_mlops/gcp/vertexai/components/__init__.py
@@ -3,4 +3,4 @@ from .gretel import gretel_component
 from .train import train_component
 from .evaluate import evaluate_component
 from .register import register_component
-from .utils import *
+# from .utils import *
