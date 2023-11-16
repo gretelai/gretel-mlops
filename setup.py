3a9e53a7ea939089f5560375080015f228a9daf1
mvansegbroeck <mvansegbroeck@gmail.com>
2023-11-16 15:50:11 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="gretel_sm_pipelines",
     version="0.0.1",
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=[
