287da433ba5f200c957ecf24f519a40dbec5c65a
mvansegbroeck <mvansegbroeck@gmail.com>
2023-11-16 15:48:13 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     package_dir={"": "src"},
     install_requires=[
         'gretel-client',
-        'scikit-learn==1.3.0',
-        'git+https://github.com/gretelai/gretel-tuner',
-        'imblearn',
-        'xgboost',
-        'optuna'
+        # 'scikit-learn==1.3.0',
+        # 'git+https://github.com/gretelai/gretel-tuner',
+        # 'imblearn',
+        # 'xgboost',
+        # 'optuna'
     ],
     license="https://gretel.ai/license/source-available-license",
 )
