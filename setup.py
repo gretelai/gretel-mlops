71db9f56a4400c010e989a6dc37a1b5b09d0dd97
mvansegbroeck <mvansegbroeck@gmail.com>
2023-11-16 21:01:40 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ setup(
     python_requires=">=3.10",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=[
-        'numpy<1.24,>=1.20',
-    #     'gretel-client',
-    #     'scikit-learn==1.3.0',
-    #     # 'git+https://github.com/gretelai/gretel-tuner',
-    #     'imblearn',
-    #     'xgboost',
-    #     'optuna'
-    ],
+    # install_requires=[
+    #     'numpy<1.24,>=1.20',
+    #     'wrapt<1.15,>=1.11.0'
+    # #     'gretel-client',
+    # #     'scikit-learn==1.3.0',
+    # #     # 'git+https://github.com/gretelai/gretel-tuner',
+    # #     'imblearn',
+    # #     'xgboost',
+    # #     'optuna'
+    # ],
     license="https://gretel.ai/license/source-available-license",
 )
