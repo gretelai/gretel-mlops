4772bfabc39ee78cbd92b549f4d29ff89691404b
mvansegbroeck <mvansegbroeck@gmail.com>
2024-01-16 13:51:45 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gretel_mlops",
-    version="0.0.1",
+    version="0.0.2",
     python_requires=">=3.10",
     packages=find_packages("src"),
     package_dir={"": "src"},
